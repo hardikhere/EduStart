295b9c45b5826e37b23fcb0960a81547bc969478
hardikhere <web.hardikhere@gmail.com>
2023-12-04 16:01:40 +0530

    importing react

--- edustart-frontend/src/pages/_app.tsx
+++ edustart-frontend/src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Hydrate } from "react-query/hydration";
 import { ReactQueryDevtools } from "react-query/devtools";
