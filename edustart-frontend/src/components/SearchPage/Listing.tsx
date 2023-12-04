ae643bac85572696d405717df4bf5439ee4c6a0f
hardikhere <web.hardikhere@gmail.com>
2023-12-04 19:17:56 +0530

    remove unused code

--- edustart-frontend/src/components/SearchPage/Listing.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Listing = () => {
-  return <div>Listing</div>;
-};
-
-export default Listing;
---
+++ edustart-frontend/src/pages/school/[schoolId].tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SchoolProfilePage = () => {
+  return <div>[schoolId]</div>;
+};
+
+export default SchoolProfilePage;
